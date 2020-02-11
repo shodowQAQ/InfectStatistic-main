e15690fb1a7ab66b911665b04c15742f2f4ec8da
温温温 <44270148+shodowQAQ@users.noreply.github.com>
2020-02-11 21:22:39 +0800

    feat:设置好静态数据和指令

--- 021700531/src/InfectStatistic.java
+++ 021700531/src/InfectStatistic.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * InfectStatistic
@@ -12,17 +13,36 @@ import java.io.IOException;
  * @since xxx
  */
 class InfectStatistic {
-    public static void main(String[] args) throws IOException {
+	static String[] province=new String[24];
+	static int[] infected=new int[24];
+	static int[] doubt=new int[24];
+	static int[] death=new int[24];
+	static int[] cure=new int[24];
+    public static void main(String[] args) throws IOException{
+    	province[0]="全国";
+    	province[1]="";
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+        System.out.println("Enter lines of text.");
+        str = br.readLine();
+        System.out.println(str);
+        String x="15616";
+        int y=Integer.parseInt(x);
+        System.out.print(y);
+        /*
         File file = new File("log//2020-01-22.log.txt");
+        String xx;
         System.out.println(txt2String(file));
+        xx=txt2String(file);
+        */
     }
-    
     public static String txt2String(File file){
         StringBuilder result = new StringBuilder();
         try{
             BufferedReader br = new BufferedReader(new FileReader(file));//构造一个BufferedReader类来读取文件
             String s = null;
             while((s = br.readLine())!=null){//使用readLine方法，一次读一行
+            	if(!s.substring(0, 1).equals("/"))
                 result.append(System.lineSeparator()+s);
             }
             br.close();
@@ -33,3 +53,4 @@ class InfectStatistic {
     }
 }
 
+	
