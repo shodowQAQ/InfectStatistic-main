abc98e46f552d3729d301ed3073fb87afcbbc365
温温温 <44270148+shodowQAQ@users.noreply.github.com>
2020-02-11 20:26:06 +0800

    feat:读取txt文件

---
+++ 021700531/src/InfectStatistic.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * InfectStatistic
+ * TODO
+ *
+ * @author xxx
+ * @version xxx
+ * @since xxx
+ */
+class InfectStatistic {
+    public static void main(String[] args) throws IOException {
+        File file = new File("log//2020-01-22.log.txt");
+        System.out.println(txt2String(file));
+    }
+    
+    public static String txt2String(File file){
+        StringBuilder result = new StringBuilder();
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(file));//构造一个BufferedReader类来读取文件
+            String s = null;
+            while((s = br.readLine())!=null){//使用readLine方法，一次读一行
+                result.append(System.lineSeparator()+s);
+            }
+            br.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return result.toString();
+    }
+}
+
