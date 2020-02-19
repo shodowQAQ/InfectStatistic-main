ef3a789fa52de46e31c1c66d165110291c60dd31
温温温 <44270148+shodowQAQ@users.noreply.github.com>
2020-02-19 21:43:55 +0800

    feat：日期指令检测

--- 021700531/src/InfectStatistic.java
+++ 021700531/src/InfectStatistic.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 
 /**
  * InfectStatistic
@@ -17,7 +19,10 @@ class InfectStatistic {
 	/*定义静态数据省份和数据*/
 	static String[] province = {"全国", "安徽", "澳门" ,"北京", "重庆", "福建","甘肃","广东", "广西", "贵州", "海南", "河北", "河南", "黑龙江", "湖北", "湖南", "吉林","江苏", "江西", "辽宁", "内蒙古", "宁夏", "青海", "山东", "山西", "陕西", "上海","四川", "台湾", "天津", "西藏", "香港", "新疆", "云南", "浙江"};
 	static int[][] data =new int[35][4];
-   
+	SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+	static String date;
+	static String input;
+	static String output;
     /*读入txt文件*/
     public static String txt2String(File file){
         StringBuilder result = new StringBuilder();
@@ -35,39 +40,55 @@ class InfectStatistic {
         return result.toString();
     }
     
+    /*指令类*/
+    class Instructions{
+    	String [] instructions;
+    	void setInstructions(String[] a){//设置指令
+    		instructions=a;
+    	}		
+    	public boolean checkInstructions() {//判断指令是否输入成功
+    		if(instructions.length==0)
+    		{
+    			System.out.println("指令输入失败");
+    			return false;
+    		}
+    		if(!instructions[0].equals("list")) {//判断命令格式是否正确
+    			System.out.println("指令必须以list开头");
+    			return false;
+    		}
+    		for (int i=0;i<instructions.length;i++)
+    		{
+    			 if(instructions[i].equals("-date")) { //读取到-date参数
+ 					i= getDate(++i);
+ 					if(i == -1) { //说明上述步骤中发现命令行出错
+ 						System.out.println("日期输入格式错误");
+ 						return false;
+ 					}
+    			}
+    		}
+    		return true;
+    	}
+    	
+		public int getDate(int i) {
+			if(i < instructions.length) { //当下标未越界
+				date = instructions[i] + ".log.txt";
+		    }	
+			else
+			{
+				return -1;
+			}
+			return i;
+		}	
+
+    }
     public static void main(String[] args){
-        Instructions instruction=new Instructions();
+    	InfectStatistic infectstatistic=new InfectStatistic();
+        Instructions instruction=infectstatistic.new Instructions();
         instruction.setInstructions(args);
         instruction.checkInstructions();
-      //  System.out.println("Enter lines of text.");
-      //  str = br.readLine();
-      //  System.out.println(str);
-        
-        /*
-        File file = new File("log//2020-01-22.log.txt");
-        String xx;
-        System.out.println(txt2String(file));
-        xx=txt2String(file);
-        */
+
     }
+    
 }
 
-/*指令类*/
-class Instructions{
-	String [] Instructions;
-	void setInstructions(String[] a){//设置指令
-		Instructions=a;
-	}		
-	public boolean checkInstructions() {//判断指令是否输入成功
-		if(Instructions.length==0)
-		{
-			System.out.println("指令输入失败");
-			return false;
-		}
-		if(!Instructions[0].equals("list")) {//判断命令格式是否正确
-			System.out.println("指令必须以list开头");
-			return false;
-		}
-		return true;
-	}
-}	
+
